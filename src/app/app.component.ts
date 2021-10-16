947474bc2e7cf718902db128942979d9fb7d34a7
Oleg O <oleg_biblyi@epam.com>
2021-10-16 18:24:45 +0300

    add line numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { fromEvent, Subscription } from 'rxjs';
+import { PrismService } from './services/prism.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-prism';
+export class AppComponent implements OnInit, AfterViewChecked, AfterViewInit, OnDestroy {
+  @ViewChild('textArea', { static: true })
+  textArea!: ElementRef;
+  @ViewChild('codeContent', { static: true })
+  codeContent!: ElementRef;
+  @ViewChild('pre', { static: true })
+  pre!: ElementRef;
+
+  sub!: Subscription;
+  highlighted = false;
+  codeType = 'html';
+
+  form = this.fb.group({
+    content: ''
+  });
+
+  get contentControl() {
+    return this.form.get('content');
+  }
+
+  constructor(
+    private prismService: PrismService,
+    private fb: FormBuilder,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this.listenForm()
+    this.synchronizeScroll();
+  }
+
+  ngAfterViewInit() {
+    this.prismService.highlightAll();
+  }
+
+  ngAfterViewChecked() {
+    if (this.highlighted) {
+      this.prismService.highlightAll();
+      this.highlighted = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
+
+  private listenForm() {
+    this.sub = this.form.valueChanges.subscribe((val) => {
+      const modifiedContent = this.prismService.convertHtmlIntoString(val.content);
+
+      this.renderer.setProperty(this.codeContent.nativeElement, 'innerHTML', modifiedContent);
+
+      this.highlighted = true;
+    });
+  }
+
+  private synchronizeScroll() {
+    const localSub  = fromEvent(this.textArea.nativeElement, 'scroll').subscribe(() => {
+      const toTop = this.textArea.nativeElement.scrollTop;
+      const toLeft = this.textArea.nativeElement.scrollLeft;
+
+      this.renderer.setProperty(this.pre.nativeElement, 'scrollTop', toTop);
+      this.renderer.setProperty(this.pre.nativeElement, 'scrollLeft', toLeft + 0.2);
+    });
+
+    this.sub.add(localSub);
+  }
 }
